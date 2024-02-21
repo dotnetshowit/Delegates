79d945705e3ae4b32ab7a9497f8b7a4131862ecc
Priyank <Priyank@LAPTOP-BQUJONOE>
2024-02-21 23:30:40 +0530

    Add project files.

---
+++ Delegates/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Delegates.Program;
+
+namespace Delegates
+{
+   
+    internal class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int Experience { get; set; }
+        public int Salary { get; set; }
+
+        public static void PromoteEmployee(List<Employee> employees, IsEmployeePromotable delPromote)
+        {
+            foreach(Employee employee in employees)
+            {
+                if(delPromote(employee))
+                {
+                    Console.WriteLine("Promoted");
+                }
+            }
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    //public delegate void PrintName(string name);
+
+    internal class Program
+    {
+        public delegate bool IsEmployeePromotable(Employee emp);
+
+        static void Main(string[] args)
+        {
+            //PrintName delPrint = new PrintName(PrintWelcomeMessage);
+
+            //delPrint("WelCome Message");
+
+            var listEmp = new List<Employee>();
+
+            listEmp.Add(new Employee() { Experience = 6 });
+            listEmp.Add(new Employee() { Experience = 1 });
+            listEmp.Add(new Employee() { Experience = 3 });
+            listEmp.Add(new Employee() { Experience = 8 });
+
+            IsEmployeePromotable delPromote = new IsEmployeePromotable(IsEmployeeEligible);
+
+
+            Employee.PromoteEmployee(listEmp, delPromote);
+
+            Console.ReadLine();
+
+        }
+
+
+
+        //public static void PrintWelcomeMessage(string welcomeMessage) {
+
+        //    Console.WriteLine(welcomeMessage);
+
+        //}
+
+
+
+        public static bool IsEmployeeEligible(Employee emp)
+        {
+             return emp.Experience > 6;
+        }
+    }
+}
